5b41f66a01b5a5cd873d140ec5df4b252c455e36
scubaguba345 <brandon.gubitosa@gmail.com>
2020-06-08 17:28:17 -0400

    added changes to CarOwner

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,19 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def mechanics
+    cars.map {|car| car.mechanic}.uniq
+  end
+
+  def self.average_number_of_cars
+    Car.all.length / @@all.length
+  end
 
 end
